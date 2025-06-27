a5e6fa1bd19f3ad772b7d6ed1355881e20451b16
Samir Ribeiro <samir.sjr77@gmail.com>
2025-06-27 14:21:20 -0300

    Maps
    InfoWindow with street and number

--- lib/ui/map.dart
+++ lib/ui/map.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:travels/helpers/location_helper.dart';
@@ -15,20 +16,24 @@ class TravelMap extends StatefulWidget {
 class _TravelMapState extends State<TravelMap> {
   final _controller = Completer<GoogleMapController>();
 
-  Set<Marker> _markers = {};
+  final Set<Marker> _markers = {};
 
   var _cameraPosition = CameraPosition(target: LatLng(0, 0), zoom: 10);
 
-  _getCurrentLocation() async {
-    await LocationHelper.isLocationEnabled();
-    var currentPosition = await Geolocator.getCurrentPosition();
-
-    _moveCamera(
-      CameraPosition(
-        target: LatLng(currentPosition.latitude, currentPosition.longitude),
-        zoom: 15
-      ),
+  _createLocationListener() {
+    var settings = LocationSettings(
+      accuracy: LocationAccuracy.high,
+      distanceFilter: 5,
     );
+
+    Geolocator.getPositionStream(locationSettings: settings).listen((position) {
+      _moveCamera(
+        CameraPosition(
+          target: LatLng(position.latitude, position.longitude),
+          zoom: 16,
+        ),
+      );
+    });
   }
 
   _moveCamera(CameraPosition cameraPosition) async {
@@ -39,11 +44,22 @@ class _TravelMapState extends State<TravelMap> {
     controller.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
   }
 
-  _showMarker(LatLng position) {
+  _showMarker(LatLng position) async {
+    List<Placemark> placemarks = await placemarkFromCoordinates(
+      position.latitude,
+      position.longitude,
+    );
+
+    var firstPlaceMark = placemarks.first;
+
     var marker = Marker(
-        markerId: MarkerId("travel-${position.latitude}-${position.longitude}"),
+      markerId: MarkerId("travel-${position.latitude}-${position.longitude}"),
       position: position,
-      infoWindow: InfoWindow(title: "Teste")
+      infoWindow: InfoWindow(
+        title:
+            "${firstPlaceMark.thoroughfare} - ${firstPlaceMark.subThoroughfare}",
+        snippet: "Teste"
+      ),
     );
 
     setState(() {
@@ -53,7 +69,7 @@ class _TravelMapState extends State<TravelMap> {
 
   @override
   void initState() {
-    _getCurrentLocation();
+    _createLocationListener();
 
     super.initState();
   }
@@ -61,7 +77,11 @@ class _TravelMapState extends State<TravelMap> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Maps")),
+      appBar: AppBar(
+        title: Text("Maps"),
+        foregroundColor: Colors.white,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
       body: GoogleMap(
         mapType: MapType.normal,
         initialCameraPosition: _cameraPosition,
