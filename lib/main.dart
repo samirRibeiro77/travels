b7d9d1a5ad4c3165bb79d12e5800e491c1f1fd1a
Samir Ribeiro <samir.sjr77@gmail.com>
2025-06-27 14:41:12 -0300

    Firebase
    Init Firebase configs

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:travels/firebase_options.dart';
 import 'package:travels/ui/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyTravelsApp());
 }
 
